d438b25032a684ffc915e43093ecb273615d068a
collestbablu <collestbablu@gmail.com>
2019-06-04 16:53:07 +0530

    scrap data get from product table

--- application/modules/productionModule/views/scrap-grn-details.php
+++ application/modules/productionModule/views/scrap-grn-details.php
@@ -96,23 +96,17 @@ $getOrder=$orderQuery->row();
 					<th class="tdcenter"> Sl No</th>
 					<th class="tdcenter">Item Number & Description</th>
 					<th class="tdcenter">UOM</th>
-					<th class="tdcenter">Ordered Qty</th>
+					<th class="tdcenter">Scrap Qty</th>
 			        <th class="tdcenter">Remaining Qty</th>
                     <th class="tdcenter" style="display:none">Qty In Stock</th>
                      <th class="tdcenter">Receive Qty</th>
-                     <th class="tdcenter">Total Weight</th>
-                     <th class="tdcenter">Ideal Net Weight</th>
-                     <th class="tdcenter">Net Weight</th>
-                     <th style="display:none" class="tdcenter">RM rate per kg</th>
-                     <th style="display:none" class="tdcenter">Total Rm Amount</th>
-                     <th style="display:none" class="tdcenter">Labour rate per kg</th>
-                     <th style="display:none" class="tdcenter">Total labour amount</th>
-                     <th style="display:none" class="tdcenter">Total cost</th>
+                    
+                     
 				</tr>
 			</thead>
         
       <?php
-		$productQuery=$this->db->query("select *from tbl_job_work_log where job_order_no='$id'");
+		$productQuery=$this->db->query("select *from tbl_product_stock where type='13'");
 		$i=1;
 		foreach($productQuery->result() as $getProduct){
 		####### get product #######
@@ -129,7 +123,7 @@ $getOrder=$orderQuery->row();
 		##### ends #####
 		
 		####### get UOM #######
-		$productUOMQuery=$this->db->query("select *from tbl_master_data where serial_number='$getProductStock->usageunit'");
+		$productUOMQuery=$this->db->query("select *from tbl_master_data where serial_number='$getProduct->usageunit'");
 		$getProductUOM=$productUOMQuery->row();
 		####### ends ########
 		
@@ -146,7 +140,7 @@ $getOrder=$orderQuery->row();
 		?>
        <tr class="gradeX odd" role="row">
             <td class="size-60 text-center sorting_1"><?=$i;?></td>
-			<td><?=$getProductStock->sku_no;?>
+			<td><?=$getProduct->sku_no;?>&<?=$getProduct->productname;?>
               <input type="hidden"  name="productid[]" value="<?=$getProduct->part_id;?>" class="form-control">
               
             </td>
@@ -180,26 +174,13 @@ $inbountLogGRNLogQuery=$this->db->query("select SUM(qty) as rec_qty from tbl_pro
             <td>
             
             <input name="tolerance_percentage[]" id="tolerance_percentage<?=$i;?>"  type="hidden" class="form-control" value="<?=$getProductStock->tolerance_percentage;?>"/>
-            <input name="qty[]" id="qty<?=$i;?>" onchange="qtyVal(this.id)" type="text" class="form-control"<?php if($reci_qty==0){?> readonly="readonly" <?php }?> />
+            <input name="qty[]" id="qty<?=$i;?>" onchange="qtyVal(this.id)" type="text" class="form-control" />
             
             <input type="text" style="display:none" name="process_ends[]" value="1" />
             
             </td>
             
-            <td> <input class="form-control" onchange="totalWeightCal(this.id)"  style="margin-bottom:10px;width:55px;" value="" name="total_weight[]" id="total_weight<?=$i;?>"  /></td>
-            <td> <input class="form-control"   style="margin-bottom:10px;width:105px;" value="<?=$getProductPart->qty;?>" readonly="readonly" name="ideal_total_weight[]" id="ideal_total_weight<?=$i;?>"  /></td>
-   <td> <input class="form-control"   style="margin-bottom:10px;width:95px;" readonly="readonly" value="" name="weight[]" id="weight<?=$i;?>"  /></td>
-<input type="hidden" id="net_weight_cal<?=$i;?>" value="<?=$getProductStock->net_weight;?>" />
-        
-        <td style="display:none"> <input class="form-control" style="margin-bottom:10px;width:55px;" value="<?=$getProduct->rate?>" name="rate[]" id="rate<?=$i;?>" onchange="RateCal(this.id)"  /></td>
-        <td style="display:none"> <input class="form-control" style="margin-bottom:10px;width:55px;" value="" name="total_rm_rate[]" id="total_rm_rate<?=$i;?>"  /></td>
-        <td style="display:none"> <input class="form-control" style="margin-bottom:10px;width:55px;" value="" name="labour_rate[]" id="labour_rate<?=$i;?>" onchange="labourRateCal(this.id)"  /></td>
-
-        
-
-        <td style="display:none"> <input class="form-control" style="margin-bottom:10px;width:55px;" value="" name="total_labour_rate[]" id="total_labour_rate<?=$i;?>"  /></td>
-
-        <td style="display:none">  <input class="form-control" style="margin-bottom:10px; width:55px;" value="<?=$getJob->total_cost;?>" id="total_cost<?=$i;?>" name="total_cost[]"  /></td>
+          
                                            
 		</tr>
 	        <?php 
