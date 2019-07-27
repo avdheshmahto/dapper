5d929376910320318ae4131585ad1abb5c26498d
avdheshmahto <avdheshmehta12@gmail.com>
2019-07-27 20:37:45 +0530

    changes in get page for job order, grn and checking

--- application/modules/productionModule/views/getGrnOrder.php
+++ application/modules/productionModule/views/getGrnOrder.php
@@ -1,44 +1,59 @@
-<table class="table table-striped table-bordered table-hover dataTables-example1" >
-  <thead>
-    <tr>
-      <th style="display:none">Lot No.</th>
-      <th>Grn No.</th>
-      <th>Grn Date</th>
-      <th>Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-      $queryData=$this->db->query("select *from tbl_production_order_log where job_order_id='$id' and grn_type='Job Order' group by grn_no");
-        foreach($queryData->result() as $fetch_list)
-        {
-        
-      ?>
-    <tr class="gradeU record">
-      <td style="display:none">
-        <p style="display:none" id="lot_no"><?=$_GET['id'];?></p>
-        <p style="display:none" id="order_type"><?=$fetch_list->order_type;?></p>
-        <?=$fetch_list->lot_no;?>
-      </td>
-      <td><?=$fetch_list->grn_no;?></td>
-      <td><?=$fetch_list->grn_date;?></td>
-      <td><?php $pri_col='id';
-        $table_name='tbl_schedule_triggering';
-        ?>
-        <a href="#" title="GRN VIEW" data-toggle="modal" data-target="#model-view-production-log" onclick="view_production_log('<?=$fetch_list->grn_no;?>');"><i class="fa fa-eye"></i></a>&nbsp;
-        <a target="_blank" href="<?=base_url();?>productionModule/print_challan?id=<?=$fetch_list->id;?>"><img src="<?=base_url();?>assets/images/print1.png" /></a>    
-      </td>
-    </tr>
-    <?php  }?>
-    <tr class="gradeU">
-      <td style="display:none">&nbsp;</td>
-      <td>
-        <p style="display:none" id="lot_no"><?=$_GET['id'];?></p>
-        <p style="display:none" id="order_type"><?=$getsched->order_type;?></p>
-        <button style="display:none1" type="button" class="btn btn-default modalMapSpare" onclick="Order('<?=$getsched->job_order_no;?>');" data-toggle="modal" data-target="#modal-order"><img src="<?=base_url();?>assets/images/plus.png" /></button>
-      </td>
-      <td>&nbsp;</td>
-      <td>&nbsp;</td>
-    </tr>
-  </tbody>
-</table>
+
+                      <thead>
+                        <tr>
+                          <th style="display:none">Lot No.</th>
+                          <th>Grn No.</th>
+                          <th>Grn Date</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php
+                          $queryData=$this->db->query("select *from tbl_production_order_log where job_order_id='$id' and grn_type='Job Order' group by grn_no");
+                            foreach($queryData->result() as $fetch_list)
+                            {
+                            
+                          ?>
+                        <tr class="gradeU record">
+                          <td style="display:none">
+                            <p style="display:none" id="lot_no"><?=$id;?></p>
+                            <p style="display:none" id="order_type"><?=$fetch_list->order_type;?></p>
+                            <?=$fetch_list->lot_no;?>
+                          </td>
+                          <td><?=$fetch_list->grn_no;?></td>
+                          <td><?=$fetch_list->grn_date;?></td>
+                          <td>
+                            <a href="#" title="GRN VIEW" data-toggle="modal" data-target="#model-view-production-log" onclick="view_production_log('<?=$fetch_list->grn_no;?>,<?=$fetch_list->order_no;?>');"><i class="fa fa-eye"></i></a>&nbsp;
+                            
+                         
+                      <?php
+                       $poquery=$this->db->query("select *  from tbl_production_order_check where status='A' and order_no='$fetch_list->order_no'");
+                         $cntData=$poquery->num_rows();
+             
+                          if($cntData>0)
+              {
+                          ?>
+                       <button class="btn btn-default" onclick="return confirm('Please Delete Child Data First');" type="button"><i class="icon-trash"></i></button>
+                          <?php }else{?>
+                          <button class="btn btn-default delbuttonOrderGrn" id="<?=$fetch_list->grn_no ?>" type="button"><i class="icon-trash"></i></button>
+                          <?php }?>
+                         
+                            <a target="_blank" href="<?=base_url();?>productionModule/print_challan_grn?id=<?=$fetch_list->id;?>"><img src="<?=base_url();?>assets/images/print1.png" /></a>    
+                          </td>
+                        </tr>
+                        <?php  }?>
+                        <tr class="gradeU">
+                          <td style="display:none">&nbsp;</td>
+                          <td>
+                            <p style="display:none" id="lot_no"><?=$id;?></p>
+                            <p style="display:none" id="order_type"><?=$getsched->order_type;?></p>
+                            <button style="display:none1" type="button" class="btn btn-default modalMapSpare" onclick="Order('<?=$getsched->job_order_no;?>');" data-toggle="modal" data-target="#modal-order"><img src="<?=base_url();?>assets/images/plus.png" /></button>
+                          </td>
+                          <td>&nbsp;</td>
+                          <td>&nbsp;</td>
+                        </tr>
+                      </tbody>
+                      <tfoot>
+                  
+                      </tfoot>
+                    
--- application/modules/productionModule/views/getRequestRM.php
+++ application/modules/productionModule/views/getRequestRM.php
@@ -1,75 +1,88 @@
-<table class="table table-striped table-bordered table-hover dataTables-example1"  id="">
-  <thead>
-    <tr>
-      <th style="width:150px;">Request No.</th>
-      <th>Date</th>
-      <th>Qty</th>
-      <th>Status</th>
-      <th>Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-      $poquery=$this->db->query("select *from tbl_issuematrial_hdr where status='A' and po_no='$id'");
-      foreach($poquery->result() as $getPo){
-      ?>
-    <tr class="gradeC record">
-      <th><?=$getPo->request_no;?></th>
-      <th><?=$getPo->date;?></th>
-      <th>
-        <?php
-          $poquery=$this->db->query("select SUM(receive_qty) as qty from tbl_issuematrial_dtl where status='A' and inboundrhdr='$getPo->inboundid'");
-          $getQty=$poquery->row();
-          
-          // tbl_receive_matrial_grn_log query
-          
-          
-          //echo "select SUM(receive_qty) as qty from tbl_receive_matrial_grn_log where status='A' and po_no='$getPo->inboundid'";
-          
-          $poquerygrnLog=$this->db->query("select SUM(receive_qty) as qty from tbl_receive_matrial_grn_log where status='A' and po_no='$getPo->po_no'");
-          $getQtygrnLog=$poquerygrnLog->row();
-          
-          
-          ?>
-        <?=$getQty->qty;?>
-      </th>
-      <th>
-        <?php
-          if($getQty->qty==$getQtygrnLog->qty)
-          {
-          	echo "Completed";
-          }
-          elseif($getQty->qty<$getQtygrnLog->qty)
-          {
-          	echo "Partial Completed";
-          }
-          else
-          {
-          	echo "Pending";
-          }
-          
-          ?>
-      </th>
-      <th>
-        <?php /*?><button class="btn btn-default" onclick="viewPurchaseOrder(<?=$getPo->purchaseid;?>);" data-toggle="modal" data-target="#modal-6" type="button" ><i class="fa fa-eye"></i></button><?php */?>
-        <input type="hidden" id="p_n" value="<?=$getPo->po_no;?>" />
-        <button class="btn btn-default" onclick="viewRawRequest(<?=$getPo->inboundid;?>);" data-toggle="modal" data-target="#modal-rawRequest" type="button" ><i class="fa fa-eye"></i></button>
-        <a href="<?=base_url();?>productionModule/manage_jobwork_map_rm_details?id=<?=$getPo->inboundid;?>"><img src="<?=base_url();?>assets/images/click.png" height="25" width="50" /></a>
-        <a target="_blank" href="<?=base_url();?>productionModule/print_request_challan?id=<?=$getPo->inboundid;?>"><img src="<?=base_url();?>assets/images/print1.png" /></a>		
-      </th>
-    </tr>
-    <?php }?>
-    <tr class="gradeU">
-      <td>
-        <button type="button" class="btn btn-default modalMapSpare1" data-toggle="modal" data-target="#modal-6"><img src="<?=base_url();?>assets/images/plus.png" /></button> 
-      </td>
-      <td>&nbsp;</td>
-      <td>&nbsp;</td>
-      <td>&nbsp;</td>
-      <td>&nbsp;</td>
-    </tr>
-  </tbody>
-  <tfoot>
-    <!--<button  class="btn btn-default modalMapSpare" data-a="<?php echo $fetch_list->id;?>" href='#mapSpare'  type="button" data-toggle="modal" data-backdrop='static' data-keyboard='false' formid = "#mapSpareForm" id="formreset"><img src="<?=base_url();?>assets/images/plus.png" /></button>-->
-  </tfoot>
-</table>
+
+                      <thead>
+                        <tr>
+                          <th style="width:150px;">Request No.</th>
+                          <th>Date</th>
+                          <th>Qty</th>
+                          <th>Status</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php
+                          $poquery=$this->db->query("select *from tbl_issuematrial_hdr where po_no='$id'");
+                          foreach($poquery->result() as $getPo){
+                          ?>
+                        <tr class="gradeC record">
+                          <th><?=$getPo->request_no;?></th>
+                          <th><?=$getPo->date;?></th>
+                          <th>
+                            <?php
+                              $poquery=$this->db->query("select SUM(receive_qty) as qty from tbl_issuematrial_dtl where status='A' and inboundrhdr='$getPo->inboundid'");
+                              $getQty=$poquery->row();
+                              
+                              
+                              $poquerygrnLog=$this->db->query("select SUM(receive_qty) as qty from tbl_receive_matrial_grn_log where status='A' and po_no='$getPo->po_no'");
+                              $getQtygrnLog=$poquerygrnLog->row();
+                              
+                              
+                              ?>
+                            <?php echo (round($getQty->qty,3));?>
+                          </th>
+                          <th>
+                            <?php
+                              $poqueryC=$this->db->query("select SUM(remaining_qty) as qtyC from tbl_issuematrial_dtl where status='A' and inboundrhdr='$sales->inboundid'");
+                              $getQtC=$poqueryC->row();
+                              
+                              if($getPo->status=='2')
+                              {
+                              echo "Completed";
+                              }
+                              elseif($getPo->status=='3')
+                              {
+                                echo "Partital Completed";
+                              }
+                              elseif($getPo->status=='1')
+                              {
+                              echo "Force Close"; 
+                              }
+                              else
+                              {
+                                echo "Open";
+                                }
+                              ?>
+                          </th>
+                          <th>
+                         
+                            <input type="hidden" id="p_n" value="<?=$getPo->po_no;?>" />
+                            <button title="View RM request" class="btn btn-default" onclick="viewRawRequest(<?=$getPo->inboundid;?>);" data-toggle="modal" data-target="#modal-rawRequest" type="button" ><i class="fa fa-eye"></i></button>
+                            <button title="View RM Receive Log" class="btn btn-default" onclick="viewChallanLog('<?=$getPo->inboundid;?>^<?=$getPo->po_no;?>');" data-toggle="modal" data-target="#modal-ChallanLog" type="button" ><i class="fa fa-eye"></i></button>
+                            <a style="display:none" href="<?=base_url();?>productionModule/manage_jobwork_map_rm_details?id=<?=$getPo->inboundid;?>"><img src="<?=base_url();?>assets/images/click.png" height="25" width="50" /></a>
+                            <a target="_blank" href="<?=base_url();?>productionModule/print_request_challan?id=<?=$getPo->inboundid;?>"><img src="<?=base_url();?>assets/images/print1.png" /></a>    
+                          </th>
+                        </tr>
+                        <?php }?>
+                        <tr class="gradeU">
+                          <td>
+                            <?php
+                              $issueHdrQuery=$this->db->query("select *from tbl_issuematrial_hdr where po_no='$id'");
+                              $getIssueHdr=$issueHdrQuery->row();
+                              
+                              
+                              $issueHdrValQuery=$this->db->query("select *from tbl_issuematrial_hdr where job_order_no='$getIssueHdr->job_order_no'");
+                              $getIssuValeCnt=$issueHdrValQuery->num_rows();
+                              
+                              if($getIssuValeCnt=='0')
+                              {
+                              ?>
+                            <button type="button" class="btn btn-default modalMapSpare1" data-toggle="modal" data-target="#modal-6"><img src="<?=base_url();?>assets/images/plus.png" /></button> 
+                            <?php }?>
+                          </td>
+                          <td>&nbsp;</td>
+                          <td>&nbsp;</td>
+                          <td>&nbsp;</td>
+                          <td>&nbsp;</td>
+                        </tr>
+                      </tbody>
+                    
+                    
