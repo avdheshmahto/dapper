1a40d2a1405a89396ea829e987467a08e74587b5
avdheshmahto <avdheshmehta12@gmail.com>
2019-08-16 18:29:39 +0530

    changes in order grn page and order checking for qty

--- application/modules/kora/views/order-check.php
+++ application/modules/kora/views/order-check.php
@@ -1,8 +1,7 @@
 <?php
-  $orderQuery=$this->db->query("select *from tbl_job_work where job_order_no='$id'");
+  $orderQuery=$this->db->query("select * from tbl_job_work where job_order_no='$id'");
   $getOrder=$orderQuery->row();
-  
-  ?>
+?>
 <div class="panel-body">
   <div class="row">
     <div class="col-lg-12">
@@ -24,11 +23,8 @@
               <label for="po_order">Order Date.:</label>
               <input type="text" name="check_date"  class="form-control" value="<?=$getOrder->date;?>" readonly="readonly" required />
             </div>
-            <!--  <div class="col-sm-6" id="grnId" >
-              <label for="po_order">GRN No.:</label>
-                <input type="text" name="grn_no" class="form-control" required readonly="readonly" value="<?=$getHdr->grn_no;?>"  />
-                             </div> -->
           </div>
+
           <div class="form-group">
             <div class="col-sm-6">
               <label for="po_order">Check No.:</label>
@@ -38,11 +34,8 @@
               <label for="po_order">Check Date</label>
               <input type="date" name="check_date"  class="form-control" value="<?=$getOrder->order_receive_date;?>"  required />
             </div>
-            <!--  <div class="col-sm-6" id="grnId" >
-              <label for="po_order">GRN No.:</label>
-                <input type="text" name="grn_no" class="form-control" required readonly="readonly" value="<?=$getHdr->grn_no;?>"  />
-                             </div> -->
           </div>
+
         </div>
       </div>
     </div>
@@ -55,7 +48,7 @@
         <table class="table table-striped table-bordered table-hover " >
           <thead>
             <tr>
-              <th class="tdcenter"> Sl No</th>
+              <th class="tdcenter">Sl No</th>
               <th class="tdcenter">Item Number & Description</th>
               <th class="tdcenter">UOM</th>
               <th class="tdcenter">Ordered Qty</th>
@@ -81,15 +74,10 @@
             $getProductUOM=$productUOMQuery->row();
             ####### ends ########
             
-            
-            
-            
-            
             ####### get product serial #######
             $productStockSerialQuery=$this->db->query("select * from tbl_product_serial where product_id='$getProduct->part_id'");
             $getProductSerialStock=$productStockSerialQuery->row();
             ####### ends ########
-            
             
             ?>
           <tr class="gradeX odd" role="row">
@@ -99,7 +87,6 @@
             </td>
             <td><?=$getProductUOM->keyvalue;?></td>
             <?php
-              // select M.*,S.Product_id,S.quantity,S.usageunit,S.productname,S.Product_id from tbl_part_price_mapping M,tbl_product_stock S,tbl_machine MM where M.rowmatial = S.Product_id AND MM.id = M.machine_id AND MM.machine_name = $pid 
               
               $poLogQuery=$this->db->query("select D.qty as po_qty,SUM(M.qty) as mqty from tbl_quotation_purchase_order_dtl D,tbl_part_price_mapping M,tbl_machine MM where MM.machine_name = D.productid AND MM.id = M.machine_id AND D.purchaseid='$getHdr->po_no' and M.rowmatial='$getProduct->productid' AND M.type ='part'");
               $getPoQty=$poLogQuery->row();
@@ -107,6 +94,7 @@
               
               ?>
             <?php
+            
               $inbountLogGRNLogQuery=$this->db->query("select SUM(transfer_qty) as rec_qty from tbl_production_order_check where productid='$getProduct->productid' AND job_order_id = '$lot_no' and order_no='$id'");
               			$getInboundGRNLog=$inbountLogGRNLogQuery->row();
               
@@ -131,15 +119,8 @@
       </div>
     </div>
   </div>
-  <!--scrollbar-y close-->		
-  <!-- <div style="width:100%; background:#dddddd; padding-left:0px; color:#000000; border:2px solid "> -->
-  <!-- <div style="width:100%; background:white;   color:#000000;  max-height:170px; overflow-x:auto;overflow-y:auto;" id="m">
-    <table id="invoice"  style="width:100%;background:white;margin-bottom:0px;margin-top:0px;min-height:30px;" title="Invoice" class="table table-bordered blockContainer lineItemTable ui-sortable"  >
-    
-    <tr></tr>
-    </table> -->
-  <!-- </div> -->
 </div>
+
 <div class="modal-footer">
   <input type="submit" class="btn btn-sm" id="add_req" value="Save">
   <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
--- application/modules/kora/views/order-details.php
+++ application/modules/kora/views/order-details.php
@@ -1,8 +1,7 @@
 <?php
   $orderQuery=$this->db->query("select *from tbl_job_work where job_order_no='$id'");
   $getOrder=$orderQuery->row();
-  
-  ?>
+?>
 <div class="panel-body">
   <div class="row">
     <div class="col-lg-12">
@@ -25,10 +24,7 @@
               <label for="po_order">Order Date:</label>
               <input type="text" name="invoice_no"  class="form-control" value="<?=$getOrder->date;?>" readonly="readonly" required />
             </div>
-            <!--  <div class="col-sm-6" id="grnId" >
-              <label for="po_order">GRN No.:</label>
-                <input type="text" name="grn_no" class="form-control" required readonly="readonly" value="<?=$getHdr->grn_no;?>"  />
-                             </div> -->
+
           </div>
           <div class="form-group">
             <div class="col-sm-6">
@@ -39,10 +35,7 @@
               <label for="po_order">GRN Date</label>
               <input type="date" name="grn_date"  class="form-control" value="<?=$getOrder->order_receive_date;?>"  required />
             </div>
-            <!--  <div class="col-sm-6" id="grnId" >
-              <label for="po_order">GRN No.:</label>
-                <input type="text" name="grn_no" class="form-control" required readonly="readonly" value="<?=$getHdr->grn_no;?>"  />
-                             </div> -->
+ 
           </div>
         </div>
       </div>
@@ -80,15 +73,10 @@
             $getProductUOM=$productUOMQuery->row();
             ####### ends ########
             
-            
-            
-            
-            
             ####### get product serial #######
             $productStockSerialQuery=$this->db->query("select * from tbl_product_serial where product_id='$getProduct->part_id'");
             $getProductSerialStock=$productStockSerialQuery->row();
             ####### ends ########
-            
             
             ?>
           <tr class="gradeX odd" role="row">
@@ -98,7 +86,6 @@
             </td>
             <td><?=$getProductUOM->keyvalue;?></td>
             <?php
-              // select M.*,S.Product_id,S.quantity,S.usageunit,S.productname,S.Product_id from tbl_part_price_mapping M,tbl_product_stock S,tbl_machine MM where M.rowmatial = S.Product_id AND MM.id = M.machine_id AND MM.machine_name = $pid 
               
               $poLogQuery=$this->db->query("select D.qty as po_qty,SUM(M.qty) as mqty from tbl_quotation_purchase_order_dtl D,tbl_part_price_mapping M,tbl_machine MM where MM.machine_name = D.productid AND MM.id = M.machine_id AND D.purchaseid='$getHdr->po_no' and M.rowmatial='$getProduct->productid' AND M.type ='part'");
               $getPoQty=$poLogQuery->row();
@@ -119,14 +106,16 @@
             <td style="display:none"><?=$getProductSerialStock->quantity;?></td>
             <td> <input type="checkbox" name="process_ends[]" value="" /></td>
             <td>
-              <input type="hidden" name="qty_weight[]" />
-              
+
+              <input type="hidden" name="qty_weight[]" />              
               <input type="hidden" name="weight[]" />
               <input type="hidden" name="total_weight[]" />
               <input type="hidden" name="rate[]" />
               <input type="hidden" name="total_rm_rate[]" />
               <input type="hidden" name="total_labour_rate[]" />
               <input type="hidden" name="total_cost[]" />
+
+              <input type="hidden" name="rmOrdId[]" />
 
               <input name="qty[]" id="qty<?=$i;?>" onchange="qtyVal(this.id)" type="text" class="form-control"<?php if($reci_qty==0){?> readonly="readonly" <?php }?> />
             </td>
@@ -138,15 +127,8 @@
       </div>
     </div>
   </div>
-  <!--scrollbar-y close-->		
-  <!-- <div style="width:100%; background:#dddddd; padding-left:0px; color:#000000; border:2px solid "> -->
-  <!-- <div style="width:100%; background:white;   color:#000000;  max-height:170px; overflow-x:auto;overflow-y:auto;" id="m">
-    <table id="invoice"  style="width:100%;background:white;margin-bottom:0px;margin-top:0px;min-height:30px;" title="Invoice" class="table table-bordered blockContainer lineItemTable ui-sortable"  >
-    
-    <tr></tr>
-    </table> -->
-  <!-- </div> -->
-</div>
+ </div>
+
 <div class="modal-footer">
   <input type="submit" class="btn btn-sm" id="add_req" value="Save">
   <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
